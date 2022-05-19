6f002269933afbf0135ca3fe678f541bcb06f0cd
Subhan Baig <49116925+subhanbaig209@users.noreply.github.com>
2022-05-18 21:36:47 -0700

    Update test_pqueue.cc

--- test_pqueue.cc
+++ test_pqueue.cc
@@ -31,6 +31,46 @@ TEST(PQueue, great) {
   EXPECT_EQ(pq.Top(), 34);
 }
 
+// test that uses less_equal (<=) comparator
+TEST(PQueue, less_equal) {
+  PQueue<int, std::less_equal<int> > pq;
+
+  pq.Push(55);
+  pq.Push(67);
+  pq.Push(55);
+  pq.Push(3);
+  pq.Push(34);
+  pq.Push(55);
+  pq.Pop();
+
+  EXPECT_EQ(pq.Top(), 34);
+  EXPECT_EQ(pq.Size(), 5);
+  pq.Pop();
+  EXPECT_EQ(pq.Top(), 55);
+}
+// test that uses greater_equal (>=) comparator
+TEST(PQueue, greater_equal) {
+  PQueue<int, std::greater_equal<int> > pq;
+
+  pq.Push(55);
+  pq.Push(67);
+  pq.Push(55);
+  pq.Push(3);
+  pq.Push(34);
+  pq.Push(55);
+  pq.Pop();
+  pq.Pop();
+  pq.Push(10);
+  pq.Push(97);
+
+  EXPECT_EQ(pq.Top(), 97);
+  EXPECT_EQ(pq.Size(), 6);
+  pq.Pop();
+  EXPECT_EQ(pq.Top(), 55);
+}
+
+
+
 class MyClass {
  public:
   explicit MyClass(int n) : n_(n) {}
@@ -39,6 +79,7 @@ class MyClass {
  private:
   int n_;
 };
+
 
 TEST(PQueue, custom_class) {
   std::vector<MyClass> vec{MyClass(42), MyClass(23), MyClass(2), MyClass(34)};
@@ -55,12 +96,41 @@ TEST(PQueue, custom_class) {
   EXPECT_EQ(pq.Top().n(), vec[1].n());
 }
 
+// class that compares MyClass objects with greater operator
+class MyClassGreater {
+ public:
+  explicit MyClassGreater(int n) : n_(n) {}
+  bool operator < (const MyClassGreater &mc) const { return mc.n_ > n_; }
+  int n() { return n_; }
+ private:
+  int n_;
+};
 
+// test that uses MyClassGreater comparator
+TEST(PQueue, custom_class_for_max_heap) {
+  std::vector<MyClassGreater> vec{MyClassGreater(42), MyClassGreater(23), MyClassGreater(2),
+  MyClassGreater(34), MyClassGreater(15), MyClassGreater(1)};
+
+  PQueue<MyClassGreater> pq;
+  pq.Push(vec[0]);
+  pq.Push(vec[1]);
+  pq.Push(vec[2]);
+  pq.Push(vec[3]);
+  pq.Push(vec[4]);
+  pq.Push(vec[5]);
+
+  EXPECT_EQ(pq.Top().n(), vec[5].n());
+  EXPECT_EQ(pq.Size(), 6);
+  pq.Pop();
+  EXPECT_EQ(pq.Top().n(), vec[2].n());
+}
+
+/*
 TEST(PQueue, custom_class_pointer) {
   std::vector<MyClass*> vec{new MyClass(42), new MyClass(23),
                             new MyClass(2), new MyClass(34)};
 
-  PQueue<MyClass*, /* */> pq;
+  PQueue<MyClass*, /> pq;
   pq.Push(vec[0]);
   pq.Push(vec[1]);
   pq.Push(vec[2]);
@@ -71,7 +141,7 @@ TEST(PQueue, custom_class_pointer) {
   pq.Pop();
   EXPECT_EQ(pq.Top(), vec[1]);
 }
-
+*/
 int main(int argc, char *argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
