1363bf1b459ed278b7e8fc0958910588b6d9e4e1
Ryan-Vakhshoori <96512123+Ryan-Vakhshoori@users.noreply.github.com>
2022-05-19 20:35:13 -0700

    finished unzap

--- unzap.cc
+++ unzap.cc
@@ -1,21 +1,11 @@
 #include "huffman.h"
 
 int main(int argc, char* argv[]) {
-  std::string filename{"test_zap"};
-  const unsigned char val[] = {
-      0x28, 0x74, 0x2A, 0x08, 0x00, 0x00, 0x01, 0xE7, 0xFF, 0xFF,
-      0xFF, 0xEA, 0xAA, 0xAA, 0xAA, 0xAA, 0xA0, 0x00, 0x00,
-  };
-  std::ofstream ofs(filename,
+  std::ifstream ifs(argv[1], std::ios::in | std::ios::binary);
+  std::ofstream ofs(argv[2],
                     std::ios::out | std::ios::trunc | std::ios::binary);
-  ofs.write(reinterpret_cast<const char*>(val), sizeof(val));
+  Huffman unzap;
+  unzap.Decompress(ifs, ofs);
+  ifs.close();
   ofs.close();
-  Huffman test;
-  std::ifstream ifs(filename, std::ios::in | std::ios::binary);
-  std::string ofilename{"output_zap"};
-  std::ofstream ofs1(ofilename,
-                    std::ios::out | std::ios::trunc | std::ios::binary);
-  test.Decompress(ifs, ofs1);
-  ofs1.close();
-  std::remove(filename.c_str());
 }
